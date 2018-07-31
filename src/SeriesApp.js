fd7b2af3427f53330d547d284368099b54815687
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-07-31 00:53:42 -0300

    Importado a store, provider, reducer e instalado o redux devtools

--- src/SeriesApp.js
+++ src/SeriesApp.js
@@ -1,8 +1,23 @@
 import React from "react";
+
+import devtoolsEnhancer from "remote-redux-devtools";
+
+/* ROUTER É UM COMPONENTE ONDE FICARAO AS REFERENCIAS DE TODAS AS PAGINAS DA APLICAÇÃO */
 import Router from "./Router";
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import rootReducer from "./reducers";
+
+/* PASSAREMOS UMA REDUCER PARA A createStore(REDUCER) */
+const store = createStore(rootReducer, devtoolsEnhancer());
+
+/* TODO COMPONENTE QUE PRECISA TER ACESSO A ALGO DO REDUX, PRECISA ESTAR DENTRO DO PROVIDER - QUE PROVISIONARÁ A STORE */
 const SeriesApp = prop => (
-  <Router />
+  <Provider store={store}>
+    <Router />
+  </Provider>
 );
 
 export default SeriesApp;
