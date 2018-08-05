e57a9a32476925497767006296ea85f3d57bb543
kiyo_yodo <kiyoshibudokan@hotmail.com>
2018-08-05 20:09:01 -0300

    Series Page Finalizado

--- src/pages/SeriesPage.js
+++ src/pages/SeriesPage.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, FlatList } from "react-native";
+import SerieCard from "../components/SerieCard";
+
+import series from "../../series.json"
+
+const fn_isPar = numero => numero % 2 === 0;
+
 
 const SeriesPage = (props) => (
 	<View>
-		<Text>Esta é a Séries Page</Text>
+		<FlatList
+			data={ series }
+			renderItem={({ item, index }) => (
+				<SerieCard 
+					serie={item} 
+					isPrimeiraColuna={ fn_isPar(index) }
+				/>
+			)}
+			// QDO CARREGAMOS UM JSON, É ESPERADO UMA KEY DO OBJETO, PARA ISSO UTILIZAREMOS A keyExtractor - COMO A ID É UNICA SERVIRA DE KEY
+			// SE O JSON IMPORTADO, JA TIVESSE UM ATTR CHAMADO "key" NAO PRECISARIA DESTE RECURSO
+			keyExtractor={item => item.id}
+			numColumns={2}
+			ListHeaderComponent={props => (
+				<View style={estilo.jaja}>
+					<Text>Header</Text>
+				</View>
+			)}
+			ListFooterComponent={props => (
+				<View style={estilo.jaja}>
+					<Text>Footer</Text>
+				</View>
+			)}
+		/>
 	</View>
 );
 
